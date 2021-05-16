39b1da78022f042bb0efbf860fb652199adbc432
Lukatjee <lukatjee.dev@gmail.com>
2021-05-16 22:46:48 +0200

    Initial commit

---
+++ app/src/main/java/eu/lukatjee/smartcolab/Main.kt
@@ -0,0 +1,110 @@
+package eu.lukatjee.smartcolab
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
+class Main : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forgotPin = findViewById<TextView>(R.id.forgotPin); forgotPin.setOnClickListener(this)
+        val loginButton = findViewById<Button>(R.id.loginButton); loginButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+
+            R.id.loginButton -> userLogin()
+            R.id.forgotPin -> {
+
+                val usernameInput = findViewById<EditText>(R.id.fieldUsername); val usernameData = usernameInput.text.toString().trim()
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(usernameData).matches()) {
+
+                    usernameInput.error = "Invalid e-mail address"
+                    usernameInput.requestFocus()
+
+                    return
+
+                }
+
+                FirebaseAuth.getInstance().sendPasswordResetEmail(usernameData).addOnCompleteListener { task ->
+
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(this, "Email has been sent!", Toast.LENGTH_LONG).show()
+
+                    } else {
+
+                        Toast.makeText(this, "This email address is not in our database!", Toast.LENGTH_LONG).show()
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun userLogin() {
+
+        val usernameInput = findViewById<EditText>(R.id.fieldUsername); val usernameData = usernameInput.text.toString().trim()
+        val passwordInput = findViewById<EditText>(R.id.fieldPassword); val passwordData = passwordInput.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(usernameData).matches()) {
+
+            usernameInput.error = "Invalid e-mail address"
+            usernameInput.requestFocus()
+
+            return
+
+        }
+
+        if (passwordData.isEmpty()) {
+
+            passwordInput.error = "Password is required"
+            passwordInput.requestFocus()
+
+            return
+
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(usernameData, passwordData).addOnCompleteListener(this) { task ->
+
+            if (task.isSuccessful) {
+
+                intent = Intent(this, Profile::class.java)
+                startActivity(intent)
+
+                val user = FirebaseAuth.getInstance().currentUser
+
+            } else {
+
+                Toast.makeText(
+
+                    this,
+                    "Failed to log in, please check your credentials!",
+                    Toast.LENGTH_LONG
+
+                ).show()
+
+            }
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/eu/lukatjee/smartcolab/Profile.kt
@@ -0,0 +1,166 @@
+package eu.lukatjee.smartcolab
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+
+class Profile : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        getData()
+
+        val editUsernameBtn = findViewById<TextView>(R.id.changeDpBtn); editUsernameBtn.setOnClickListener(this)
+        val editEmailBtn = findViewById<TextView>(R.id.changeEmailBtn); editEmailBtn.setOnClickListener(this)
+
+    }
+
+    override fun onBackPressed() {
+
+        super.onBackPressed()
+        FirebaseAuth.getInstance().signOut()
+
+    }
+
+    override fun onClick(v: View?) {
+
+        val editDpFld = findViewById<EditText>(R.id.editUsernameField)
+        val editEmFld = findViewById<EditText>(R.id.editEmailField)
+
+        when (v?.id) {
+
+            R.id.changeDpBtn -> {
+
+                when (editDpFld.visibility) {
+
+                    EditText.GONE -> { editDpFld.visibility = EditText.VISIBLE; when (editEmFld.visibility) { EditText.VISIBLE -> { editEmFld.visibility = EditText.GONE; editEmFld.text.clear() }}}
+                    else -> editDpFld.visibility = EditText.GONE
+
+                }
+
+                editDpFld.setOnKeyListener { _, keyCode, event ->
+
+                    if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+
+                        val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser; val userIpt = editDpFld.text.toString().trim()
+
+                        if (userIpt.isNotEmpty()) {
+
+                            val profileUpdate = UserProfileChangeRequest.Builder().setDisplayName(userIpt).build()
+                            user!!.updateProfile(profileUpdate).addOnCompleteListener {
+
+                                editDpFld.visibility = EditText.GONE
+                                editDpFld.text.clear()
+
+                                FirebaseAuth.getInstance().signOut()
+                                intent = Intent(this, Main::class.java)
+                                startActivity(intent)
+
+                            }
+
+                            return@setOnKeyListener true
+
+                        } else {
+
+                            editDpFld.error = "Invalid username"
+                            editDpFld.requestFocus()
+
+                            return@setOnKeyListener false
+
+                        }
+
+                    }
+
+                    false
+
+                }
+
+            }
+
+            R.id.changeEmailBtn -> {
+
+                when (editEmFld.visibility) {
+
+                    EditText.GONE -> { editEmFld.visibility = EditText.VISIBLE; when (editDpFld.visibility) { EditText.VISIBLE -> { editDpFld.visibility = EditText.GONE; editDpFld.text.clear() }}}
+                    else -> editEmFld.visibility = EditText.GONE
+
+                }
+
+                editEmFld.setOnKeyListener { _, keyCode, event ->
+
+                    if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+
+                        val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+                        val userIpt = editEmFld.text.toString().trim()
+
+                        if (Patterns.EMAIL_ADDRESS.matcher(userIpt).matches()) {
+
+                            user!!.updateEmail(userIpt).addOnCompleteListener {
+
+                                editEmFld.visibility = EditText.GONE
+                                editEmFld.text.clear()
+
+                                FirebaseAuth.getInstance().signOut()
+                                intent = Intent(this, Main::class.java)
+                                startActivity(intent)
+
+
+                            }
+
+                            return@setOnKeyListener true
+
+                        } else {
+
+                            editEmFld.error = "Invalid e-mail address"
+                            editEmFld.requestFocus()
+
+                            return@setOnKeyListener false
+
+                        }
+
+                    }
+
+                    false
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun getData() {
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        var name : String? = "N/A"
+        var email : String?
+
+        user.let {
+
+            if (user!!.displayName!!.isNotEmpty()) { name = user.displayName }
+            email = user.email
+
+        }
+
+        val displayNameVw = findViewById<TextView>(R.id.displayNameVw)
+        displayNameVw.text = name; displayNameVw.invalidate()
+
+        val emailAddressVw = findViewById<TextView>(R.id.emailVw)
+        emailAddressVw.text = email; emailAddressVw.invalidate()
+
+    }
+
+}
